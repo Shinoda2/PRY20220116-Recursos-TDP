ab1bfefd477c646f159f5db14c5b6050a8fe2eb7
Keiji Shinoda <k.shinoda.2000@hotmail.com>
2022-09-29 17:45:12 -0500

    Listar Medicamentos

--- lib/screens/list_medicine.dart
+++ lib/screens/list_medicine.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:pry20220116/models/medicine.dart';
 import 'package:pry20220116/screens/profile.dart';
 import 'package:pry20220116/screens/profile_patient.dart';
 import 'package:pry20220116/widgets/nav_bar_patient.dart';
+import 'package:pry20220116/widgets/navigation_bar_patient.dart';
 
 class ListMedicine extends StatefulWidget{
   const ListMedicine({Key? key}) : super(key: key);
@@ -12,6 +15,7 @@ class ListMedicine extends StatefulWidget{
 }
 
 class _ListMedicine extends State<ListMedicine>{
+  String name = '';
   bool isGrid=true;
   List<Medicine> medicines = Medicine.generateMedicine();
 
@@ -19,51 +23,104 @@ class _ListMedicine extends State<ListMedicine>{
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: NavBarPatient(),
-      appBar: AppBar(
-        title: Text('MEDICAMENTOS'),
-        centerTitle: true,
-        actions: [
-          IconButton(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('MEDICAMENTOS'),
+          actions: [
+            IconButton(
             icon: const Icon(Icons.person),
-            onPressed: () {
+            onPressed:(){
               Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => ProfilePatient()));
-            },
-          ),
-        ],
-      ),
-      body: Container(
-        color: Colors.blueGrey,
-        child: GridView.builder(
-           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
-            ),
-            itemCount: medicines.length,
-            itemBuilder: (context, index) {
-              final item= medicines[index];
-              return GridTile(
-                child: Image.asset(
-                  '${item.icon}',
-                  height: 20,
-                  fit: BoxFit.cover,
-              ),
-                footer: Text(item.name,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                    color: Colors.amber
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              );
-            },
+                builder: (BuildContext context) => ProfilePatient()
+              ));
+            }),
+          ],
         ),
-      ),
-    );
+        body: Column(
+          children: <Widget>[
+            TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search), hintText: 'Buscar...'),
+                  onChanged: (val) {
+                    setState(() {
+                      name = val;
+                    });
+                  },
+                ),
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection('medicamento').snapshots(),
+              builder: (context, snapshots) {
+                return (snapshots.connectionState == ConnectionState.waiting)
+                    ? Center(
+                  child: CircularProgressIndicator(),
+                )
+                    : ListView.builder(
+                  scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: snapshots.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      var data = snapshots.data!.docs[index].data()
+                      as Map<String, dynamic>;
+
+                      if (name.isEmpty) {
+                        return ListTile(
+                          title: Text(
+                            data['nombre'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text('Stock: '+
+                            data['stock'].toString(),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(data['imagen']),
+                          ),
+                        );
+                      }
+                      if (data['nombre']
+                          .toString()
+                          .toLowerCase()
+                          .startsWith(name.toLowerCase())) {
+                        return ListTile(
+                          title: Text(
+                            data['nombre'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text('Stock: '+
+                            data['stock'].toString(),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(data['imagen']),
+                          ),
+                        );
+                      }
+                      return Container();
+                    });
+              },
+            ),
+          ],
+    ),
+      bottomNavigationBar: NavigationBarPatient(),);
   }
-
-
-
-}
+  }
