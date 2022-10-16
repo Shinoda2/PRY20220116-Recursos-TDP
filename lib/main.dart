b0eb6b74ee0da2745d87d98eb736c89026667b6e
Jose Carlos <josecarlos_dk@hotmail.com>
2022-10-16 08:34:45 -0500

    commit prueba
    commit prueba

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,32 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
-import 'dart:io';
-import 'package:flutter/material.dart';
 import 'package:pry20220116/screens/option.dart';
 //import 'package:pry20220116/widgets/camera_sc.dart';
 //import 'package:path/path.dart';
 //import 'package:path_provider/path_provider.dart';
-import 'package:pry20220116/screens/option.dart';
 
 /*void main() {
   runApp(const MyApp());
 }*/
 
-Future<void> main()async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: FirebaseOptions(apiKey: "AIzaSyDAkVaZFWRkA2rWnH3fKMIOEH2wpc9CSG0", appId: "1:340493700277:android:37e3aea13ec36f5ba342fc", messagingSenderId: "340493700277", projectId: "wisha-database")
-  );
+      options: FirebaseOptions(
+          apiKey: "AIzaSyDAkVaZFWRkA2rWnH3fKMIOEH2wpc9CSG0",
+          appId: "1:340493700277:android:37e3aea13ec36f5ba342fc",
+          messagingSenderId: "340493700277",
+          projectId: "wisha-database"));
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     var pixelRatio = window.devicePixelRatio;
     var logicalScreenSize = window.physicalSize / pixelRatio;
     var logicalWidth = logicalScreenSize.width;
@@ -36,63 +34,61 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Chatbot',
       theme: ThemeData(
-        textTheme: TextTheme(
-          headline1: TextStyle(
-            fontSize: logicalWidth / 14, // 360 / 18
-            fontWeight: FontWeight.w600, // Semi-bold
-            color: Colors.black,
+          textTheme: TextTheme(
+            headline1: TextStyle(
+              fontSize: logicalWidth / 14, // 360 / 18
+              fontWeight: FontWeight.w600, // Semi-bold
+              color: Colors.black,
+            ),
+            headline2: TextStyle(
+              fontSize: logicalWidth / 18, // 360 / 18
+              fontWeight: FontWeight.w500, // Medium
+              color: Colors.black,
+            ),
+            headline3: TextStyle(
+              fontSize: logicalWidth / 20, // 360 / 16
+              fontWeight: FontWeight.w500, // Medium
+              color: Colors.black,
+            ),
+            headline4: TextStyle(
+              fontSize: logicalWidth / 20, // 360 / 16
+              fontWeight: FontWeight.w600, // Semi-bold
+              color: Colors.black,
+            ),
+            subtitle1: TextStyle(
+              fontSize: logicalWidth / 22, // 360 / 16
+              fontWeight: FontWeight.w500, // Medium
+              color: Colors.black,
+            ),
+            subtitle2: TextStyle(
+              fontSize: logicalWidth / 22, // 360 / 16
+              fontWeight: FontWeight.w400, // Regular
+              color: Colors.black,
+            ),
+            bodyText1: TextStyle(
+              fontSize: logicalWidth / 24, // 360 / 14
+              fontWeight: FontWeight.w400, // Regular
+              color: Colors.black,
+            ),
+            bodyText2: TextStyle(
+              fontSize: logicalWidth / 26, // 360 / 12
+              fontWeight: FontWeight.w400, // Regular
+              color: Colors.black,
+            ),
+            caption: TextStyle(
+              fontSize: logicalWidth / 26, // 360 / 12
+              fontWeight: FontWeight.w400, // Regular
+              color: Colors.black54,
+            ),
+            overline: TextStyle(
+              fontSize: logicalWidth / 26, // 360 / 10
+              fontWeight: FontWeight.w500, // Medium
+              color: Colors.white,
+            ),
           ),
-          headline2: TextStyle(
-            fontSize: logicalWidth / 18, // 360 / 18
-            fontWeight: FontWeight.w500, // Medium
-            color: Colors.black,
-          ),
-          headline3: TextStyle(
-            fontSize: logicalWidth / 20, // 360 / 16
-            fontWeight: FontWeight.w500, // Medium
-            color: Colors.black,
-          ),
-          headline4: TextStyle(
-            fontSize: logicalWidth / 20, // 360 / 16
-            fontWeight: FontWeight.w600, // Semi-bold
-            color: Colors.black,
-          ),
-          subtitle1: TextStyle(
-            fontSize: logicalWidth / 22, // 360 / 16
-            fontWeight: FontWeight.w500, // Medium
-            color: Colors.black,
-          ),
-          subtitle2: TextStyle(
-            fontSize: logicalWidth / 22, // 360 / 16
-            fontWeight: FontWeight.w400, // Regular
-            color: Colors.black,
-          ),
-          bodyText1: TextStyle(
-            fontSize: logicalWidth / 24, // 360 / 14
-            fontWeight: FontWeight.w400, // Regular
-            color: Colors.black,
-          ),
-          bodyText2: TextStyle(
-            fontSize: logicalWidth / 26, // 360 / 12
-            fontWeight: FontWeight.w400, // Regular
-            color: Colors.black,
-          ),
-          caption: TextStyle(
-            fontSize: logicalWidth / 26, // 360 / 12
-            fontWeight: FontWeight.w400, // Regular
-            color: Colors.black54,
-          ),
-          overline: TextStyle(
-            fontSize: logicalWidth / 26, // 360 / 10
-            fontWeight: FontWeight.w500, // Medium
-            color: Colors.white,
-          ),
-        ),
-        fontFamily: 'Montserrat',
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder()
-        )
-      ),
+          fontFamily: 'Montserrat',
+          inputDecorationTheme:
+              InputDecorationTheme(border: OutlineInputBorder())),
       debugShowCheckedModeBanner: false,
 
       home: Option(),
@@ -104,4 +100,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
--- lib/models/paciente.dart
+++ lib/models/paciente.dart
@@ -1,26 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Paciente{
+class Paciente {
   final String? nombre;
   final int? edad;
   final String? direccion;
   final int? dni;
   final String? docid;
-  final Timestamp? fecha_hora;
+  final Timestamp? fechaHora;
 
-  const Paciente(
-      {this.nombre,
-        this.edad,
-        this.direccion,
-        this.dni,
-        this.docid,
-        this.fecha_hora,
-      });
+  const Paciente({
+    this.nombre,
+    this.edad,
+    this.direccion,
+    this.dni,
+    this.docid,
+    this.fechaHora,
+  });
 
   factory Paciente.fromFirestore(
-      DocumentSnapshot<Map<String, dynamic>> snapshot,
-      SnapshotOptions? options,
-      ){
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
     final data = snapshot.data();
     return Paciente(
       nombre: data?['nombre'],
@@ -28,7 +28,7 @@ class Paciente{
       direccion: data?['direccion'],
       dni: data?['dni_paciente'],
       docid: data?['uid'],
-      fecha_hora: data?['fecha_hora'],
+      fechaHora: data?['fecha_hora'],
     );
   }
 }
